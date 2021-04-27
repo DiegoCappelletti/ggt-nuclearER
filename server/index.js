aac23230d224e8e5eb55bcf8fa1c84cb8202fb69
Diego Cappelletti <diegocappelletti97@gmial.com>
2021-04-27 15:57:58 +0200

    Modificata sctruttura cartelle, ora incentrato sul server; creato tuto fino al main (pagina 2)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+//Rutes Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', express.static('static'));
+
+app.use((req, res) => {
+    res.status(404);
+    res.json({success: false, message: 'Non trovato'});
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port,() => {
     console.log(`Server running at ${port} port...`)
